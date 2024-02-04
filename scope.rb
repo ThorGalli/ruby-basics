be1cb311449d1eba049bbbdc7f4f964c08446934
Thor Galli <gallithor@gmail.com>
2024-02-04 14:23:51 -0300

    add comments to scope example

--- scope.rb
+++ scope.rb
@@ -1,7 +1,10 @@
 a = 5
 
+# this do => end is called a block
 3.times do |n|
-  a += 5
+  a += 5 # a is available here
+  b = 4 # b is initialized here
 end
 
 puts a
+puts b # b is not available here, it will throw an error
